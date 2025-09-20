69850555ad70dcb2b9d971325a2f59e4db30d2b0
KjetilAl <kjetilalmas@pm.me>
2025-09-20 11:36:50 +0200

    Update main_bot.py
    stray )

--- main_bot.py
+++ main_bot.py
@@ -175,7 +175,6 @@ async def on_message(message):
         message.guild, message.author, config, game_info
     )
                     game_key, message.author.display_name, game_info, player_stats
-                )
 
                 if not post_message and config.get("create_acknowledgement"):
                     acknowledgement = config["create_acknowledgement"](message.author.display_name, game_info)
